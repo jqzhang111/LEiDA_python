8b0f8c670226def976aed5c6fcda1fd7dad4fb16
jqzhang111 <105253814+jqzhang111@users.noreply.github.com>
2022-05-14 22:30:09 +0800

    Add files via upload

---
+++ 02_emp_kmeans.py
@@ -0,0 +1,76 @@
+# LEiDA(Cabral 2017. Sci Rep.)-PART2: K-means and centroids for each brain states
+# author: zhangjiaqi(Smile.Z), CASIA, Brainnetome
+import numpy as np 
+from scipy.signal import hilbert
+from scipy.spatial.distance import cosine
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import davies_bouldin_score
+import pandas as pd
+from sklearn.decomposition import PCA
+import os
+from validclust import ValidClust
+
+
+# get centroids for each brain states and sort by probability
+# V1: (nsubjects * T) * N
+# k: best cluster number
+
+def EMP_BrainStates(V1, k):
+    X = []
+    for i in range(V1.shape[0]):
+        X.append(V1[i])
+    km = KMeans(n_clusters=k)
+    km.fit(X)
+
+    centroids = km.cluster_centers_
+
+    count = pd.Series(km.labels_).value_counts()
+    center = pd.DataFrame(km.cluster_centers_)
+    r= pd.concat([center, count], axis=1)
+    r.to_csv("centroids.csv")
+
+    plt.clf()
+    vec = PCA(n_components=2).fit_transform(X)
+    df2 = pd.DataFrame(vec)
+    df2['labels'] = km.labels_
+    visual_vec = []
+    for m in range(k):
+        visual_vec[m] = df3[df2['labels'] == m]
+        plt.plot(visual_vec[m][0], visual_vec[m][1])
+    plt.savefig('kmeans_visualize.png')
+
+    return centroids
+
+
+if __name__ == '__main__':
+    path_mdd = '/share/home/zhangjiaqi/2022Project/HOPF/02_LEiDA_Empircal/step1_get_dFC_V1/V1/MDD/'
+    path_hc = '/share/home/zhangjiaqi/2022Project/HOPF/02_LEiDA_Empircal/step1_get_dFC_V1/V1/HC/'
+    mdd_file = os.listdir(path_mdd)
+    hc_file = os.listdir(path_hc)
+    
+    V1 = np.zeros((20*230, 246))
+    
+    i = 0
+    for file in hc_file:
+        path = path_hc+file
+        vec = np.loadtxt(path)
+        for j in range(vec.shape[0]):
+            V1[i, :] = vec[j]
+            i = i+1
+
+    i = 0
+    for file in mdd_file:
+        path = path_mdd+file
+        vec = np.loadtxt(path)
+        for j in range(vec.shape[0]):
+            V1[i, :] = vec[j]
+            i = i+1
+
+    center = EMP_BrainStates(V1, 3)
+    np.savetxt('centroids.txt', center, delimiter=' ')
+
+
---
+++ 02_get_best_k.py
@@ -0,0 +1,55 @@
+import numpy as np 
+from scipy.signal import hilbert
+from scipy.spatial.distance import cosine
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import davies_bouldin_score
+import pandas as pd
+from sklearn.decomposition import PCA
+import os
+from validclust import ValidClust
+
+
+# get Best K
+# V1: (nsubjects * T) * N
+# M: each k run M times for average score
+
+def Decide_K(V1):
+    X = []
+    for i in range(V1.shape[0]):
+        X.append(V1[i])
+    vclust = ValidClust(k=list(range(3, 11)), methods = ['kmeans'])
+    cvi_vals = vclust.fit_predict(X)
+    cvi_vals.to_csv('DecideK/cluster.csv')
+    vclust.plot()
+    plt.savefig('DecideK/cluster.png')
+    
+    
+if __name__ == '__main__':
+    path_mdd = '/share/home/zhangjiaqi/2022Project/HOPF/02_LEiDA_Empircal/step1_get_dFC_V1/V1/MDD/'
+    path_hc = '/share/home/zhangjiaqi/2022Project/HOPF/02_LEiDA_Empircal/step1_get_dFC_V1/V1/HC/'
+    mdd_file = os.listdir(path_mdd)
+    hc_file = os.listdir(path_hc)
+    
+    V1 = np.zeros((20*230, 246))
+    
+    i = 0
+    for file in hc_file:
+        path = path_hc+file
+        vec = np.loadtxt(path)
+        for j in range(vec.shape[0]):
+            V1[i, :] = vec[j]
+            i = i+1
+
+    i = 0
+    for file in mdd_file:
+        path = path_mdd+file
+        vec = np.loadtxt(path)
+        for j in range(vec.shape[0]):
+            V1[i, :] = vec[j]
+            i = i+1
+
+    Decide_K(V1)
